8163c6d785914d66e5672cfba3eac69b5c540ecd
muba <macbook@Macbooks-MacBook-Air.local>
2021-10-02 21:51:14 -0500

    w1d6

---
+++ w1d6/ascii.js
@@ -0,0 +1,67 @@
+window.onload = function () {
+    "use strict";
+  
+    let defaultValue = "";
+    let animationMoves = ANIMATIONS["Blank"];
+    let idx;
+    let running = false;
+    let delayTime = 250;
+    const slowTime = 250;
+    const turboTime = 50;
+  
+    const textArea = document.getElementById("text-area");
+    const startBtn = document.getElementById("start");
+    const stopBtn = document.getElementById("stop");
+    const animation = document.getElementById("animation");
+    const sizes = document.getElementById("fontsize");
+    const turbo = document.getElementById("turbo");
+  
+    startBtn.onclick = function () {
+      defaultValue = textArea.value;
+      stopBtn.disabled = false;
+      startBtn.disabled = true;
+      if (animationMoves) {
+        textArea.value = animationMoves[(idx = 0)];
+        running = true;
+        setTimeout(updateAnimation, delayTime);
+      } else textArea.value = "";
+    };
+  
+    stopBtn.onclick = function () {
+      clearTimeout();
+      running = false;
+      stopBtn.disabled = true;
+      startBtn.disabled = false;
+      textArea.value = defaultValue;
+    };
+  
+    animation.onchange = function () {
+      animationMoves = ANIMATIONS[animation.value].split("=====\n");
+    };
+  
+    sizes.onchange = function () {
+      const sizesMap = {
+        Tiny: "8pt",
+        Small: "10pt",
+        Medium: "12pt",
+        Large: "14pt",
+        "Extra Large": "16pt",
+        XXL: "18pt",
+      };
+  
+      if (textArea.style.fontSize !== sizesMap[sizes.value])
+        textArea.style.fontSize = sizesMap[sizes.value];
+    };
+  
+    turbo.onchange = function () {
+      delayTime = turbo.checked ? turboTime : slowTime;
+    };
+  
+    function updateAnimation() {
+      if (running) {
+        textArea.value = animationMoves[idx++];
+        idx %= animationMoves.length;
+        setTimeout(updateAnimation, delayTime);
+      }
+    }
+  };
